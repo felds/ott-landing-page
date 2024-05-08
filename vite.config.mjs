d6ab4f355b141c36043fe984de9c9fa80966ef52
Felds Liscia <dev@felds.com.br>
2024-05-08 16:37:19 -0300

    remove absolute urls

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,6 @@ import { resolve } from "node:path"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-    base: "https://trancoso.org/",
     build: {
         rollupOptions: {
             input: {
